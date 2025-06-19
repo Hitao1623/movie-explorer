294e9ef935e4ba0987670a2f3f2be089c11398da
Hitao1623 <hollywang23@hotmail.com>
2025-06-19 12:57:52 -0400

    redirectory

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,7 +1,7 @@
 import React, { use } from "react";
 import { useFavorites } from "../contexts/FavoritesContext";
 import { useAuth } from "../contexts/AuthContext";
-import "../styles/Favorites.css";
+import "../styles/pages/Favorites.css";
 
 export default function Favorites() {
   const { user } = useAuth();
@@ -25,32 +25,24 @@ export default function Favorites() {
   return (
     <div className="favorites-container">
       <h2>Your Favorite Movies</h2>
-      
-      {favorites.length === 0 ? (
+
+      {favorites.length === 0 ?
         <p>No favorite movies added yet.</p>
-      ) : (
-        <ul className="favorites-list">
+      : <ul className="favorites-list">
           {favorites.map((fav) => (
             <li key={fav.movieId} className="favorite-item">
-              <img
-                src={`https://image.tmdb.org/t/p/w200${fav.movieData.poster_path}`}
-                alt={fav.movieData.title}
-                className="favorite-poster"
-              />
+              <img src={`https://image.tmdb.org/t/p/w200${fav.movieData.poster_path}`} alt={fav.movieData.title} className="favorite-poster" />
               <div className="favorite-details">
                 <h3>{fav.movieData.title}</h3>
                 <p>{fav.movieData.release_date}</p>
-                <button 
-                  onClick={() => removeFavorite(fav.movieId)}
-                  className="remove-button"
-                >
+                <button onClick={() => removeFavorite(fav.movieId)} className="remove-button">
                   Remove
                 </button>
               </div>
             </li>
           ))}
         </ul>
-      )}
+      }
     </div>
   );
 }
